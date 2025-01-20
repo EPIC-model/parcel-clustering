eeb3ac639715e91ad50fe98b689a74d946f201d8
Matthias Frey <mf248@st-andrews.ac.uk>
2025-01-20 18:44:01 +0000

    findloc does not behave the same with cce as with gfortran

--- src/netcdf/netcdf_timings.f90
+++ src/netcdf/netcdf_timings.f90
@@ -119,15 +119,25 @@ contains
     !::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
     subroutine write_netcdf_timings(ncfname)
-        character(len=*), intent(in) :: ncfname
-        logical                      :: l_exist
-        integer                      :: n, m, n_timers
+        character(len=*), intent(in)  :: ncfname
+        logical                       :: l_exist
+        integer                       :: n, m, n_timers
+        double precision, allocatable :: wbuffer(:)
+        integer, allocatable          :: nbuffer(:)
 
         n_timers = size(timings)
-        call mpi_blocking_reduce(timings(1:n_timers)%wall_time, MPI_MAX, world)
-        call mpi_blocking_reduce(timings(1:n_timers)%n_calls, MPI_MAX, world)
+
+        allocate(wbuffer(1:n_timers))
+        allocate(nbuffer(1:n_timers))
+        wbuffer(1:n_timers) = timings(1:n_timers)%wall_time
+        nbuffer(1:n_timers) = timings(1:n_timers)%n_calls
+
+        call mpi_blocking_reduce(wbuffer, MPI_MAX, world)
+        call mpi_blocking_reduce(nbuffer, MPI_MAX, world)
 
         if (world%rank /= world%root) then
+            deallocate(wbuffer)
+            deallocate(nbuffer)
             return
         endif
 
@@ -144,9 +154,10 @@ contains
         n_writes = n_writes + 1
 
         do n = 1, n_timers
-            m = findloc(nc_dset(:)%name, value = "wtime-" // trim(int2string(n)), dim=1)
+            m = find_loc("wtime-", n)
 
             if (m == 0) then
+                print *, "NetCDF variable wtime-" // trim(int2string(n)) // " not found."
                 cycle
             endif
 #ifndef NDEBUG
@@ -154,14 +165,15 @@ contains
                 call mpi_exit_on_error("Timer names disagree.")
             endif
 #endif
-            call write_netcdf_scalar(ncid, nc_dset(m)%varid, timings(n)%wall_time, &
+            call write_netcdf_scalar(ncid, nc_dset(m)%varid, wbuffer(n), &
                                      n_writes, l_serial=.true.)
         enddo
 
         do n = 1, n_timers
-            m = findloc(nc_dset(:)%name, value = "ncalls-" // trim(int2string(n)), dim=1)
+            m = find_loc("ncalls-", n)
 
             if (m == 0) then
+                print *, "NetCDF variable ncalls-" // trim(int2string(n)) // " not found."
                 cycle
             endif
 
@@ -170,12 +182,34 @@ contains
                 call mpi_exit_on_error("Timer names disagree.")
             endif
 #endif
-            call write_netcdf_scalar(ncid, nc_dset(m)%varid, int(timings(n)%n_calls), &
+            call write_netcdf_scalar(ncid, nc_dset(m)%varid, nbuffer(n), &
                                      n_writes, l_serial=.true.)
         enddo
 
         call close_netcdf_file(ncid, l_serial=.true.)
 
+        deallocate(wbuffer)
+        deallocate(nbuffer)
+
     end subroutine write_netcdf_timings
+
+    !::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    ! Replaces the intrinsic Fortran routine which does not behave with CCE like the GCC version
+    ! m = findloc(nc_dset(1:n_timers)%name, value = "ncalls-" // trim(int2string(n)), dim=1)
+    function find_loc(s, n) result(r)
+        character(*), intent(in) :: s
+        integer,      intent(in) :: n
+        integer                  :: m, r
+
+        r = 0
+        do m = 1, size(nc_dset(:)%name)
+            if (nc_dset(m)%name == s // trim(int2string(n))) then
+                r = m
+                exit
+            endif
+        enddo
+    end function find_loc
+
 
 end module netcdf_timings
