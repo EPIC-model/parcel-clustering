c1814266715080da0d6c9f7fa0128b48ff3ce0c6
Matthias Frey <mf248@st-andrews.ac.uk>
2025-01-24 08:31:49 +0000

    retabl benchmark random bash scripts

--- scripts/benchmark_random/run_random.sh
+++ scripts/benchmark_random/run_random.sh
@@ -2,79 +2,79 @@
 
 run_jobs() {
 
-	# 22 Jan 2025
-	# https://stackoverflow.com/a/6212408
-	# https://stackoverflow.com/a/12128447
+    # 22 Jan 2025
+    # https://stackoverflow.com/a/6212408
+    # https://stackoverflow.com/a/12128447
 
 
-	local fname=${1}
+    local fname=${1}
 
-	# "gnu" or "cray"
-	local compiler=${2}
+    # "gnu" or "cray"
+    local compiler=${2}
 
-	mkdir -p $compiler; cd $compiler
+    mkdir -p $compiler; cd $compiler
 
-	local subcomm="false"
+    local subcomm="false"
 
-	local nrepeat=${3}
-	local niter=${4}
-	local nx=${5}
-	local ny=${6}
-	local lx=${7}
-	local ly=${8}
-	local begin=${9}
-	local end=${10}
-	local subcomm=${11}
+    local nrepeat=${3}
+    local niter=${4}
+    local nx=${5}
+    local ny=${6}
+    local lx=${7}
+    local ly=${8}
+    local begin=${9}
+    local end=${10}
+    local subcomm=${11}
 
-	echo "--------------------------------"
-	echo "Run jobs with following options:"
-	echo "fname    = $fname"
-	echo "compiler = $compiler"
-	echo "nrepeat  = $nrepeat"
-	echo "niter    = $niter"
-	echo "nx       = $nx"
-	echo "ny       = $ny"
-	echo "lx       = $lx"
-	echo "ly       = $ly"
-	echo "begin    = $begin"
-	echo "end      = $end"
-	if ! test "$subcomm" = "true"; then
-		subcomm="false"
-	fi
-	echo "subcomm  = $subcomm"
-	echo "--------------------------------"
+    echo "--------------------------------"
+    echo "Run jobs with following options:"
+    echo "fname    = $fname"
+    echo "compiler = $compiler"
+    echo "nrepeat  = $nrepeat"
+    echo "niter    = $niter"
+    echo "nx       = $nx"
+    echo "ny       = $ny"
+    echo "lx       = $lx"
+    echo "ly       = $ly"
+    echo "begin    = $begin"
+    echo "end      = $end"
+    if ! test "$subcomm" = "true"; then
+        subcomm="false"
+    fi
+    echo "subcomm  = $subcomm"
+    echo "--------------------------------"
 
-	for i in $(seq $begin 1 $end); do
-		ntasks=$((2**i))
-		nodes=$((ntasks/128))
-	
-		# 2 March 2024
-	    # https://stackoverflow.com/a/10415158
-	    nodes=$((nodes==0 ? 1 : nodes))
-	    echo "Submit job with $ntasks tasks on $nodes nodes using the $compiler version"
-	
-		fn="submit_random_nx_${nx}_ny_${ny}_nodes_${nodes}.sh"
-		cp ../$fname $fn
-		sed -i "s:JOBNAME:$compiler-random:g" $fn
-		sed -i "s:COMPILER:$compiler:g" $fn
+    for i in $(seq $begin 1 $end); do
+        ntasks=$((2**i))
+        nodes=$((ntasks/128))
+    
+        # 2 March 2024
+        # https://stackoverflow.com/a/10415158
+        nodes=$((nodes==0 ? 1 : nodes))
+        echo "Submit job with $ntasks tasks on $nodes nodes using the $compiler version"
+    
+        fn="submit_random_nx_${nx}_ny_${ny}_nodes_${nodes}.sh"
+        cp ../$fname $fn
+        sed -i "s:JOBNAME:$compiler-random:g" $fn
+        sed -i "s:COMPILER:$compiler:g" $fn
 
-		sed -i "s:NREPEAT:$nrepeat:g" $fn
-		sed -i "s:NODES:$nodes:g" $fn
-		sed -i "s:NTASKS:$ntasks:g" $fn
-		sed -i "s:--niter NITER:--niter $niter:g" $fn
-		sed -i "s:NX:$nx:g" $fn
-	    sed -i "s:NY:$ny:g" $fn
-	    sed -i "s:--lx LX:--lx $lx:g" $fn
-	    sed -i "s:--ly LY:--ly $ly:g" $fn
-	    sed -i "s:--xlen LX:--xlen $lx:g" $fn
-	    sed -i "s:--ylen LY:--ylen $ly:g" $fn
+        sed -i "s:NREPEAT:$nrepeat:g" $fn
+        sed -i "s:NODES:$nodes:g" $fn
+        sed -i "s:NTASKS:$ntasks:g" $fn
+        sed -i "s:--niter NITER:--niter $niter:g" $fn
+        sed -i "s:NX:$nx:g" $fn
+        sed -i "s:NY:$ny:g" $fn
+        sed -i "s:--lx LX:--lx $lx:g" $fn
+        sed -i "s:--ly LY:--ly $ly:g" $fn
+        sed -i "s:--xlen LX:--xlen $lx:g" $fn
+        sed -i "s:--ylen LY:--ylen $ly:g" $fn
 
-		sed -i "s:SUBCOMM:$subcomm:g" $fn
-	
-		sbatch $fn
-	done
-	
-	cd ..
+        sed -i "s:SUBCOMM:$subcomm:g" $fn
+    
+        sbatch $fn
+    done
+    
+    cd ..
 }
 
 # Argument order:
@@ -91,10 +91,16 @@ run_jobs() {
 # subcomm
 
 # 1 node to 8 nodes
-run_jobs "submit_random.sh" "cray" 1 5 256 512 80 160 7 10 "false"
+run_jobs "submit_random.sh" "cray" 10 5 256 512 80 160 7 10 "false"
 
 # 2 nodes to 32 nodes
-run_jobs "submit_random.sh" "cray" 1 5 512 512 160 160 8 12 "false"
+run_jobs "submit_random.sh" "cray" 10 5 512 512 160 160 8 12 "false"
 
 # 8 nodes to 128 nodes
-run_jobs "submit_random.sh" "cray" 1 5 1024 1024 320 320 10 14 "false"
+run_jobs "submit_random.sh" "cray" 10 5 1024 1024 320 320 10 14 "false"
+
+
+## 1 node to 8 nodes
+#run_jobs "submit_random.sh" "gnu" 10 5 256 512 80 160 7 10 "false"
+
+
--- scripts/benchmark_random/submit_random.sh
+++ scripts/benchmark_random/submit_random.sh
@@ -27,7 +27,7 @@ if test "COMPILER" = "gnu"; then
     module load PrgEnv-gnu
     module load cray-hdf5-parallel
     module load cray-netcdf-hdf5parallel
-	module load cray-dsmml
+    module load cray-dsmml
     module load cray-openshmemx
 
     export NETCDF_C_DIR=$NETCDF_DIR
@@ -41,14 +41,14 @@ elif test "COMPILER" = "cray"; then
     module load cray-hdf5-parallel/1.12.2.1
     module load cray-netcdf-hdf5parallel/4.9.0.1
     module load cray-dsmml/0.2.2
-	module load cray-openshmemx/11.5.7
+    module load cray-openshmemx/11.5.7
 
-	# load latest modules
-	module load cpe/23.09
+    # load latest modules
+    module load cpe/23.09
     export NETCDF_C_DIR=$CRAY_NETCDF_HDF5PARALLEL_DIR/crayclang/14.0
     export NETCDF_FORTRAN_DIR=$CRAY_NETCDF_HDF5PARALLEL_DIR/crayclang/14.0
     export FC=ftn
-	export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
 fi
 
 
@@ -66,74 +66,74 @@ PATH=${EXE_DIR}:$PATH
 
 sbcast --compress=none ${EXE_DIR}/benchmark_random /tmp/benchmark_random
 for i in $(seq 1 NREPEAT); do
-	srun --nodes=NODES \
-		 --ntasks=NTASKS \
-		 --unbuffered \
-		 --distribution=block:block \
-		 /tmp/benchmark_random \
- 		 --nx NX \
- 		 --ny NY \
- 		 --nz 32 \
-		 --lx LX \
- 		 --ly LY \
- 		 --lz 10.0 \
- 		 --xlen LX \
- 		 --ylen LY \
- 		 --zlen 10.0 \
- 		 --min_vratio 20.0 \
- 		 --n_per_cell 20 \
- 		 --niter NITER \
- 		 --shuffle \
- 		 --ncfname "COMPILER-shmem-random-nx-NX-ny-NY-nodes-NODES.nc" \
- 		 --graph-type "shmem"
-	for g in "p2p" "rma"; do
-    	srun --nodes=NODES \
-			 --ntasks=NTASKS \
-			 --unbuffered \
-        	 --distribution=block:block \
-			 --hint=nomultithread \
-         	 /tmp/benchmark_random \
-         	 --nx NX \
-         	 --ny NY \
-         	 --nz 32 \
-         	 --lx LX \
-         	 --ly LY \
-	         --lz 10.0 \
-	         --xlen LX \
-    	     --ylen LY \
-	         --zlen 10.0 \
-	         --min_vratio 20.0 \
-	         --n_per_cell 20 \
-	         --niter NITER \
-	         --shuffle \
-	         --ncfname "COMPILER-$g-random-nx-NX-ny-NY-nodes-NODES.nc" \
-    	     --graph-type "$g"
+    srun --nodes=NODES \
+         --ntasks=NTASKS \
+         --unbuffered \
+         --distribution=block:block \
+         /tmp/benchmark_random \
+         --nx NX \
+         --ny NY \
+         --nz 32 \
+         --lx LX \
+         --ly LY \
+         --lz 10.0 \
+         --xlen LX \
+         --ylen LY \
+         --zlen 10.0 \
+         --min_vratio 20.0 \
+         --n_per_cell 20 \
+         --niter NITER \
+         --shuffle \
+         --ncfname "COMPILER-shmem-random-nx-NX-ny-NY-nodes-NODES.nc" \
+         --graph-type "shmem"
+    for g in "p2p" "rma"; do
+        srun --nodes=NODES \
+             --ntasks=NTASKS \
+             --unbuffered \
+             --distribution=block:block \
+             --hint=nomultithread \
+             /tmp/benchmark_random \
+             --nx NX \
+             --ny NY \
+             --nz 32 \
+             --lx LX \
+             --ly LY \
+             --lz 10.0 \
+             --xlen LX \
+             --ylen LY \
+             --zlen 10.0 \
+             --min_vratio 20.0 \
+             --n_per_cell 20 \
+             --niter NITER \
+             --shuffle \
+             --ncfname "COMPILER-$g-random-nx-NX-ny-NY-nodes-NODES.nc" \
+             --graph-type "$g"
 
-		if test "SUBCOMM" = "true"; then
-			srun --nodes=NODES \
-            	 --ntasks=NTASKS \
-	             --unbuffered \
-	             --distribution=block:block \
-	             --hint=nomultithread \
-	             /tmp/benchmark_random \
-	             --nx NX \
-	             --ny NY \
-	             --nz 32 \
-	             --lx LX \
-	             --ly LY \
-	             --lz 10.0 \
-	             --xlen LX \
-	             --ylen LY \
-	             --zlen 10.0 \
-	             --min_vratio 20.0 \
-	             --n_per_cell 20 \
-	             --niter NITER \
-	             --shuffle \
-	             --ncfname "COMPILER-$g-random-nx-NX-ny-NY-nodes-NODES-subcomm.nc" \
-	             --graph-type "$g" \
-				 --subcomm
-		fi
-	done
+        if test "SUBCOMM" = "true"; then
+            srun --nodes=NODES \
+                 --ntasks=NTASKS \
+                 --unbuffered \
+                 --distribution=block:block \
+                 --hint=nomultithread \
+                 /tmp/benchmark_random \
+                 --nx NX \
+                 --ny NY \
+                 --nz 32 \
+                 --lx LX \
+                 --ly LY \
+                 --lz 10.0 \
+                 --xlen LX \
+                 --ylen LY \
+                 --zlen 10.0 \
+                 --min_vratio 20.0 \
+                 --n_per_cell 20 \
+                 --niter NITER \
+                 --shuffle \
+                 --ncfname "COMPILER-$g-random-nx-NX-ny-NY-nodes-NODES-subcomm.nc" \
+                 --graph-type "$g" \
+                 --subcomm
+        fi
+    done
 done
 
 wait
