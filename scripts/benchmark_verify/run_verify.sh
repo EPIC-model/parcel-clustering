c59cad6e4ffa7e5e8cc8cd78be955266f769ad62
Matthias Frey <mf248@st-andrews.ac.uk>
2025-01-28 08:40:31 +0000

    add if

--- scripts/benchmark_verify/run_verify.sh
+++ scripts/benchmark_verify/run_verify.sh
@@ -4,6 +4,7 @@ fname="submit_verify.sh"
 
 n_samples=1000
 seed=42
+l_enable_caf="false"
 
 for c in "gnu" "cray"; do
     mkdir "$c"
@@ -27,17 +28,19 @@ for c in "gnu" "cray"; do
 done
 
 # Run Coarray Fortran
-cd "cray"
-mkdir "caf"
-cd "caf"
-cp "../../$fname" .
+if test "$l_enable_caf" = "true"; then
+    cd "cray"
+    mkdir "caf"
+    cd "caf"
+    cp "../../$fname" .
 
-sed -i "s:#SBATCH --job-name=JOBNAME:#SBATCH --job-name=cray-caf:g" $fname
-sed -i "s:COMPILER:cray-caf:g" $fname
-sed -i "s:GRAPH_TYPE:caf:g" $fname
-sed -i "s:N_SAMPLES:$n_samples:g" $fname
-sed -i "s:SEED:$seed:g" $fname
+    sed -i "s:#SBATCH --job-name=JOBNAME:#SBATCH --job-name=cray-caf:g" $fname
+    sed -i "s:COMPILER:cray-caf:g" $fname
+    sed -i "s:GRAPH_TYPE:caf:g" $fname
+    sed -i "s:N_SAMPLES:$n_samples:g" $fname
+    sed -i "s:SEED:$seed:g" $fname
 
-echo "Submit job caf with cray. Running $n_samples samples with seed $seed."
-sbatch $fname
-cd ..
+    echo "Submit job caf with cray. Running $n_samples samples with seed $seed."
+    sbatch $fname
+    cd ..
+fi
