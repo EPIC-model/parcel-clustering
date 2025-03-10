678fd958921649dd94c52e6e770c3978662bd22d
Matthias Frey <mf248@st-andrews.ac.uk>
2025-03-10 15:47:40 +0000

    update cirrus.sh

--- scripts/cirrus.sh
+++ scripts/cirrus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 gnu_bin="/work/e710/e710/mf248/gnu/bin"
-intel_mpt_bin="/work/e710/e710/mf248/intel-mpt/bin"
+#intel_mpt_bin="/work/e710/e710/mf248/intel-mpt/bin"
 
-declare -a bins=("$gnu_bin" "$intel_mpt_bin")
-declare -a compilers=("gnu" "intel-mpt")
-declare -a enable_caf=("no" "no")
+declare -a bins=("$gnu_bin") # "$intel_mpt_bin")
+declare -a compilers=("gnu") # "intel-mpt")
+declare -a enable_caf=("no") # "no")
 
 ntasks_per_node=36
